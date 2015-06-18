4d520b2baf87d45dc492c7f7eaec6f75f2beadf5
stephanmg <stephan@syntaktischer-zucker.de>
2015-06-18 10:24:31 +0200

    Added a testsuite, upgraded dependencies and updated README.

---
+++ VRL-NeuronalTopologyImporter-Plugin/src/test/java/edu/gcsc/vrl/neti/test/NeuronalTopologyImporterTestsuite.java
@@ -0,0 +1,51 @@
+/// package's name
+package edu.gcsc.vrl.neti.test;
+
+/// imports
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @brief NETI testsuite
+ * @author stephanmg <stephan@syntaktischer-zucker.de>
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({})
+public class NeuronalTopologyImporterTestsuite {
+
+	/**
+	 * @brief before classes are set up
+	 * @throws Exception 
+	 */
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	/**
+	 * @brief after classes are setu p
+	 */
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	/**
+	 * @brief setup stage
+	 * @throws Exception 
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @brief teardown stage
+	 * @throws Exception 
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+}
